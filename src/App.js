0d0fed9d982fc494c806e8eec0899c4464dee526
khoodehui <dehui@MacBook-Pro.local>
2021-11-15 20:44:37 +0800

    Implement basic logic to save boards to local storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
 
 import Home from './components/Home'
 import Foo from './components/Foo'
+import { useDispatch } from 'react-redux'
+import { initBoardsNewUser, initBoardsFromStorage } from './reducers/boardsReducer'
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const savedBoardsData = window.localStorage.getItem("boards");
+    if (savedBoardsData) {
+      dispatch(initBoardsFromStorage(savedBoardsData))
+    } else {
+      dispatch(initBoardsNewUser())
+    }
+  }, [dispatch])
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
--- src/reducers/boardsReducer.js
+++ src/reducers/boardsReducer.js
@@ -1,19 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const sampleBoard = {
+  id: 'sample',
+  name: 'Sample Board',
+  desc: 'Sample desc'
+}
+
+const saveBoardsState = (boards) => {
+  window.localStorage.setItem("boards", JSON.stringify(boards))
+}
+
 const boardsSlice = createSlice({
   name: 'boards',
   initialState: {
-    boards: [{ id: 'sample', name: 'Sample Board', desc: 'Sample Desc' }],
+    boards: [],
   },
   reducers: {
+    initBoardsNewUser: (state) => {
+      state.boards.push(sampleBoard)
+      saveBoardsState(state.boards)
+    },
+    initBoardsFromStorage: (state, action) => {
+      state.boards = JSON.parse(action.payload)
+    },
     newBoard: (state, action) => {
       state.boards.push(action.payload)
+      saveBoardsState(state.boards)
     },
     deleteBoard: (state, action) => {
       state.boards = state.boards.filter(board => board.id !== action.payload)
+      saveBoardsState(state.boards)
     },
   },
 })
 
-export const { newBoard, deleteBoard } = boardsSlice.actions
+export const { initBoardsNewUser, initBoardsFromStorage, newBoard, deleteBoard } = boardsSlice.actions
 export default boardsSlice.reducer
