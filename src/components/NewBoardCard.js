5f5e1c247170329e2d7e74bb9137d7f6f64b29d1
khoodehui <dehui@MacBook-Pro.local>
2021-11-16 12:29:07 +0800

    Adjust NewBoardCard component

--- src/components/NewBoardCard.js
+++ src/components/NewBoardCard.js
@@ -1,7 +1,7 @@
 import { v4 as uuid } from 'uuid'
 import { useDispatch } from 'react-redux'
 import { newBoard } from '../reducers/boardsReducer'
-import { Card, CardContent, Grid, Icon} from '@mui/material'
+import { Card, CardContent, Grid, Icon, Typography } from '@mui/material'
 
 const NewBoardCard = () => {
   const dispatch = useDispatch()
@@ -17,9 +17,21 @@ const NewBoardCard = () => {
 
   return (
     <Grid item xs={12} sm={6} md={4}>
-      <Card variant='outlined' onClick={handleNewBoard}>
+      <Card
+        variant='outlined'
+        onClick={handleNewBoard}
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+          justifyContent: 'center',  
+        }}
+      >
         <CardContent>
-          <Icon color='primary'>add_circle</Icon>
+          <Icon color='primary' sx={{ fontSize: 60, width: '100%' }}>
+            add_circle
+          </Icon>
+          <Typography color='text.secondary'>New Board</Typography>
         </CardContent>
       </Card>
     </Grid>
